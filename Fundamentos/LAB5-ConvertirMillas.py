b9e72986ab2772265de34e1714e8bc7bed6f34dc
Aldahir <aldahir0603@hotmail.com>
2025-12-06 12:20:38 -0600

    Variables - Conversion de Millas

---
+++ Fundamentos/LAB5-ConvertirMillas.py
@@ -0,0 +1,8 @@
+# LAB-5 CONVERTIR MILLAS A KILOMETROS Y CONVERTIR KILOMETROS A MILLAS.
+kilometers = 12.25
+milles = 7.38
+milles_to_kilometers = kilometers / 1.61
+kilometers_to_milles = milles / 1.61
+
+print(milles, "millas son", round(milles_to_kilometers,2), "kilometros")
+print(kilometers, "kilometros son", round(kilometers_to_milles,2), "milles")
