cdbd66ccbc0c1ff0d5c768bdf725dcc6dc7a0125
Aldahir <aldahir0603@hotmail.com>
2025-12-06 12:26:40 -0600

    If-else - Evaluar si un numero es mayor o igual a 100.

---
+++ Condicionales/LAB7 - PreguntasyRespuestas.py
@@ -0,0 +1,7 @@
+# LAB7 Preguntas y Respuestas Estructura de control que evalua si un numero es mayor o igual a 100.
+
+n = int(input("Dame un numero: "))
+if n >= 100:
+    print(f'El numero" {n} "es MAYOR.')
+else:
+    print(f'El numero" {n} "es MENOR.')
