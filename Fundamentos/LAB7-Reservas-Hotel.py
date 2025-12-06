65d32a20e6b4f55878ee7868021640d5f1531554
Aldahir <aldahir0603@hotmail.com>
2025-12-06 12:22:26 -0600

    Variables - Reservas de Hotel

---
+++ Fundamentos/LAB7-Reservas-Hotel.py
@@ -0,0 +1,28 @@
+# Sistema para reservar hoteles.
+
+# Mensaje de bienvenida.
+print("****** SISTEMA DE RESERVAS DE HOTELES ******")
+
+# Definicion de variables.
+nombre_cliente = "Laura Martinez"
+dias_estancia = 5
+tarifa_diaria = 1200.00
+vista_al_mar = True
+
+# Mostrar el detalle de la reserva
+print('Cliente:', nombre_cliente)
+print('Dias de estancia:', dias_estancia)
+print('Tarifa diaria:', tarifa_diaria)
+print('Vista', vista_al_mar)
+
+# Modificaciones
+dias_estancia = 7
+tarifa_diaria = 1000.00
+vista_al_mar = False
+
+# Mostrar el detalle de la reserva
+print()
+print('Cliente:', nombre_cliente)
+print('Dias de estancia:', dias_estancia)
+print('Tarifa diaria:', tarifa_diaria)
+print('Vista', vista_al_mar)
