eee466320e7ddac4f1282279cf54f8e65ca6e2af
Aldahir <aldahir0603@hotmail.com>
2025-12-06 12:19:54 -0600

    Variables - TIERRA DE MANZANAS

---
+++ Fundamentos/LAB4-Manzanas.py
@@ -0,0 +1,9 @@
+# LAB-4 MANEJO DE VARIABLES CON TIERRA DE MANZANAS.
+john = 3
+mary = 5
+adan = 6
+total_manzanas = (john + mary + adan)
+
+print("TIERRA DE LAS MANZANAS")
+print("Jhon tiene" ,john, "Manzanas. " "Maria tiene" ,mary, "Manzanas. " "Adan tiene" ,adan, "Manzanas." , end="")
+print("\nEl total de manzanas son:" , (total_manzanas))
