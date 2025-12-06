2dfc680e1c0364e43ae5728b943624b69e9641c2
Aldahir <aldahir0603@hotmail.com>
2025-12-06 12:21:50 -0600

    Variables - Funcion Input

---
+++ Fundamentos/LAB6-EntradasySalidasSimples.py
@@ -0,0 +1,21 @@
+# LAB-6 ENTRADAS Y SALIDAS SIMPLES OPERACIONES MATEMATICAS.
+print("Ingresa el valor de a: ")
+a = float(input())
+print("Ingresa el valor de b: ")
+b = float(input())
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+
+print("OPERACION SUMA")
+print(a, "+" ,b, "=", suma)
+print("OPERACION RESTA")
+print(a, "-" ,b, "=", resta)
+print("OPERACION MULTIPLICACION")
+print(a, "*" ,b, "=", multiplicacion)
+print("OPERACION DIVISION")
+print(a, "/" ,b, "=", division)
+
+print("\nEso es todo!")
