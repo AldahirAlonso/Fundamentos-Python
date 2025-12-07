8653ff5c195f9325cd6d943bcdb9a5cd68e040f2
Aldahir <aldahir0603@hotmail.com>
2025-12-07 15:17:53 -0600

    Ejercicio 3 - Funcion Range (2, 8, 3)

---
+++ Iterativas/For-FuncionRango.py
@@ -0,0 +1,7 @@
+print("Funcion range con DOS argumentos")
+for i in range(2, 8):
+    print("The value of is is currently", i)
+
+print("\nFuncion range con TRES argumentos")
+for i in range(2, 8, 3):
+    print("The value of is is currently", i)
