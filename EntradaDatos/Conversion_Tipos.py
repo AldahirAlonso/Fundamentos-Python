8b0ff7203f5cf272911e06ce26038fe9c4814d04
Aldahir <aldahir0603@hotmail.com>
2025-12-13 23:45:32 -0600

    Conversion de Tipos de Datos

---
+++ EntradaDatos/Conversion_Tipos.py
@@ -0,0 +1,20 @@
+# Programa: Convertir tipos de datos.
+
+print("Numero original cadena.")
+numero_cadena = "25"
+print(numero_cadena)
+
+# Convertir la cadena a un entero.
+print("Conversion a entero.")
+numero_entero = int(numero_cadena)
+print(numero_entero)
+
+# Convertir el entero a flotante.
+print("Conversion a flotante.")
+numero_flotante = float(numero_entero)
+print(numero_flotante)
+
+# Convertir flotante a cadena.
+print("Conversion a cadena de nuevo.")
+numero_texto = str(numero_flotante)
+print(numero_texto)
