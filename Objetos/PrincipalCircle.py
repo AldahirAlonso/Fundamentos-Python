bd8cfc8a05e2eaa8b86af5ee3ec441787c0256f5
Aldahir <aldahir0603@hotmail.com>
2025-12-31 23:38:09 -0600

    Objetos - Herencia

---
+++ Objetos/PrincipalCircle.py
@@ -0,0 +1,13 @@
+from CircleModule import Circle
+
+radius1 = 2
+radius2 = 5
+radius3 = 7
+
+
+circle1 = Circle(radius1)
+circle1.printCircumference()
+circle2 = Circle(radius2)
+circle2.printCircumference()
+circle3 = Circle(radius3)
+circle3.printCircumference()
