4d0761c4627adb0613afa91215f79b54d755e2cf
Aldahir <aldahir0603@hotmail.com>
2025-12-31 23:35:14 -0600

    Objetos - Secuencial.

---
+++ Objetos/3.4.3-Demo-Secuencial.py
@@ -0,0 +1,14 @@
+radius1 = 2
+radius2 = 5
+radius3 = 7
+
+pi = 3.14
+
+circumferenceValue = pi * radius1 * 2
+print ("Circumference of a circle with a radius of " + str(radius1) + " is " + str(circumferenceValue))
+
+circumferenceValue = pi * radius2 * 2
+print ("Circumference of a circle with a radius of " + str(radius2) + " is " + str(circumferenceValue))
+
+circumferenceValue = pi * radius3 * 2
+print ("Circumference of a circle with a radius of " + str(radius3) + " is " + str(circumferenceValue))
